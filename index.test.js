04c90125278e796a2326be19fae0680a3c1a9a67
Gabriel Nuñez <20231871@itla.edu.do>
2025-04-15 19:31:07 -0400

    Update test workflow and scripts for clarity and consistency

--- index.test.js
+++ index.test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { JSDOM } = require('jsdom');
 
-test('Debe contener el título Hola Mundo 👋', () => {
+test('Hola Mundo 👋', () => {
   const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
   const dom = new JSDOM(html);
   const h1 = dom.window.document.querySelector('h1');
